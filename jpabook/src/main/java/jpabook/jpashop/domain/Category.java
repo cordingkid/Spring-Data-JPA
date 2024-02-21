d54f5d37a782e991266784d39d8685a0741b9e16
codingkid <jjg1014@naver.com>
2024-02-21 23:12:46 +0900

    feat: Category 추가

---
+++ jpabook/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent; //self
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>(); //self
+
+    @ManyToMany // 이건 절대 안씀 근데 예제라서 쓰는거
+    @JoinTable(
+            name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+}
