2d2d151e219121825b13b1e436ea19211a8413d3
codingkid <jjg1014@naver.com>
2024-03-04 18:10:13 +0900

    feat: JPQL 페치조인, 벌크연산, Named쿼리 사용 연습

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -15,38 +15,66 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
 
             Member member = new Member();
-            member.setUsername("관리자");
+            member.setUsername("회원1");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
-            member.setTeam(team);
+            member.setTeam(teamA);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
             member2.setAge(10);
             member2.setType(MemberType.ADMIN);
-            member2.setTeam(team);
+            member2.setTeam(teamA);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member3.setAge(10);
+            member3.setType(MemberType.ADMIN);
+            member3.setTeam(teamB);
 
 
             em.persist(member);
             em.persist(member2);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
+            // 벌크 연산 예제 FLUSH 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
 
-            List<?> resultList = em.createQuery("""
-                    select
-                        function('group_concat', m.username)
-                    from Member m
-                    where m.username = :username
-                    """, String.class).setParameter("username", "관리자1").getResultList();
 
-            System.out.println("resultList = " + resultList);
+//            List<Team> resultList = em.createQuery("""
+//                    select
+//                        t
+//                    from Team t
+//                    join fetch t.members
+//                    """, Team.class)
+//                    .getResultList();
+//
+//            System.out.println(resultList.size());
+//
+//            for (Team t : resultList) {
+//                System.out.println("team = " + t.getName() + "members=>" + t.getMembers());
+//            }
+
+            // 네임드 쿼리 사용법
+//            em.createNamedQuery("Member.findByUsername", Member.class)
+//                            .setParameter("username", "회원1")
+//                            .getResultList();
+
+//            System.out.println("resultList = " + resultList);
 
 
             tx.commit();
