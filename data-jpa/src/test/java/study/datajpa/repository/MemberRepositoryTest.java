bd41f2f67278804636d559a76dca0efd155b4090
codingkid <jjg1014@naver.com>
2024-03-20 21:40:03 +0900

    feat: Spring Data JPA 사용 Repository 생성 및 테스트

---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 테스트_멤버() throws Exception{
+        // given
+        Member member = new Member("회원1");
+        Member savedMember = memberRepository.save(member);
+        // when
+        Member findMember = memberRepository.findById(savedMember.getId()).get(); // 원래 이렇게 쓰면 안되는데 연습용이라
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
