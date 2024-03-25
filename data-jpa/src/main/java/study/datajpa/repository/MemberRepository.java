c374ccb58746580f583dc4e795a9576e08894d6b
codingkid <jjg1014@naver.com>
2024-03-25 21:51:17 +0900

    feat: Spring Data JPA 생성및 검증

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 구현하지 않아도 동작한다?
+    // 이게 쿼리 매소드 기능이다.
+    List<Member> findByUsername(String username);
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,6 +23,10 @@ class MemberRepositoryTest {
 
     @Test
     public void 테스트_멤버() throws Exception{
+
+        // memberRepository.getClass() = class jdk.proxy3.$Proxy126
+        // 이 인터페이스를 보고 Spring Data JPA가 구현 클래스를 만들어서 꽂아 버린거다!
+        System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
         // given
         Member member = new Member("회원1");
         Member savedMember = memberRepository.save(member);
@@ -30,5 +37,37 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() throws Exception{
+        // Spring Data Repository 검증
+
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get(); // 옵셔널 이렇게 쓰면 안되는데 테스트여서 썻다.
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
