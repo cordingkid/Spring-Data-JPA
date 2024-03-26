f1a6a68e692a11e0007e89edec8fe9789f971452
codingkid <jjg1014@naver.com>
2024-03-26 22:22:42 +0900

    feat: Member 엔티티 네임드 쿼리 작성

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -9,6 +9,10 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter @Setter
 @NoArgsConstructor(access = PROTECTED)
 @ToString(of = {"id", "username", "age"}) // 안하는게 좋은데 연습이라 했음
+// NamedQuery 연습 실무에서 잘 안쓴다고 함.
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username")
 public class Member {
 
     @Id
@@ -24,6 +28,11 @@ public class Member {
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     public Member(String username, int age, Team team) {
