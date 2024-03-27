e5f172ee7ddbeb89dd591b8ee4bb8d5be131e117
codingkid <jjg1014@naver.com>
2024-03-27 22:05:20 +0900

    test: 순수 JPA 페이징과 정렬 테스트

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,5 +1,6 @@
 package study.datajpa.repository;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ import static org.assertj.core.api.Assertions.*;
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Autowired EntityManager em;
 
     @Test
     public void 테스트_멤버() throws Exception{
@@ -86,5 +89,35 @@ class MemberJpaRepositoryTest {
         List<Member> result = memberJpaRepository.findByUsername("AAA");
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(member1);
+    }
+
+    @Test
+    public void paging() throws Exception{
+        // given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 1;
+        int limit = 3;
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> result = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // 페이지 계산 공식 적용...
+        // totalPAge \= totalCount / size ...
+        // 마지막 페이지...
+        // 최초 페이지
+
+        // then
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
     }
 }
