04327850e5260dc7f51b9d4bf51789bee258fd29
codingkid <jjg1014@naver.com>
2024-03-29 23:46:16 +0900

    feat: Spring Data JPA 페이징 및 정렬 과 벌크 연산

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,10 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -40,7 +44,31 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByNames(@Param("names") Collection<String> names);
 
     List<Member> findListByUsername(String username); // 컬렉션
+
     Member findMemberByUsername(String username); // 단던
+
     Optional<Member> findOptionalMemberByUsername(String username); // 단건 Optional
 
+    /**
+     * 페이징
+     * import org.springframework.data.domain.Page;
+     * import org.springframework.data.domain.Pageable;
+     *
+     * 이런식으로 카운트 쿼리를 분리해서 성능을 최적화 할 수 있다.
+     */
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    /**
+     * Slice 페이징
+     */
+//    Slice<Member> findByAge(int age, Pageable pageable);
+
+    /**
+     * bulk연산
+     * @Modifying 이 있어야
+     * executeUpdate이걸 호출 한다. 이게 없으면 singleResult 이런걸 호출한다.
+     */
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
